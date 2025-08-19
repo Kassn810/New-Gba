8b287bf8a0fc025f583fc305fd422af769d40f13
Kassn810 <Noah6k@outlook.com>
2025-08-19 17:02:38 +0000

    New main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./src/App.jsx";  // must match exactly
+import App from "./App.jsx";  relative import, must be in the same folder
 
+// Attach React app to the div with id="root" in index.html
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
 
 
