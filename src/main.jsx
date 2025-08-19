088aa7683ac41ab333ad5dd1ac3fc6067e88d9f2
Kassn810 <Noah6k@outlook.com>
2025-08-19 16:59:07 +0000

    Ficing frontend 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";  // must match exactly
+import App from "./src/App.jsx";  // must match exactly
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
